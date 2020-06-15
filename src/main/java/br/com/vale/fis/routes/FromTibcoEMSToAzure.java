c2cba550d0e013902a541707d82e2825cd3d093a
Marco Aurelio <c0609186@vale.com>
2020-06-15 16:20:40 -0300

    Correção de commit

---
+++ src/main/java/br/com/vale/fis/routes/FromTibcoEMSToAzure.java
@@ -0,0 +1,61 @@
+package br.com.vale.fis.routes;
+
+import br.com.vale.fis.components.ValeTibcoEMSComponent;
+import br.com.vale.fis.log.EventCode;
+import br.com.vale.fis.log.ValeLog;
+import br.com.vale.fis.log.ValeLogger;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FromTibcoEMSToAzure extends RouteBuilder {
+	
+		@Value("${tibco.queueIn}")
+		private String tibcoQueueIn;
+
+		@Value("${tibco.queueOut}")
+		private String tibcoQueueOut;
+	
+		@Value("${tibco.host}")
+		private String host;
+	
+		@Value("${tibco.user}")
+		private String user;
+	
+		@Value("${tibco.password}")
+		private String password;
+
+		@Value("${azure.endpoint}")
+		private String endpoint;
+		
+		@Value("${azure.autorizationKey}")
+		private String autorizationKey;
+		
+		@Value("${azure.soapAction}")
+		private String soapAction;
+
+  @Override
+  public void configure() throws Exception {
+  
+	  onException(Exception.class)
+	      .handled(true)
+	      .bean(ValeLog.class, "logging(" + EventCode.E950 + ", ${exception.message})");
+    
+	  from("tibco:".concat(tibcoQueueOut)) 
+	    .routeId("FromTibcoEMSToAzure")
+		.setHeader(ValeLogger.ROUTE_ID.getValue()).simple("${routeId}")
+		.bean(ValeLog.class, "logging(" + EventCode.V001 + ", Start)")
+		.setHeader("SOAPAction",simple(soapAction))
+	    .setHeader(HttpHeaders.AUTHORIZATION,simple(autorizationKey))
+	    .transform(simple("${body.replace('<getMasterDataResponse>', '<getMasterDataResponse xmlns=\"http://www.vale.com/EH/EH20160010_01/GetMasterData\">')}"))
+		.setHeader(Exchange.CONTENT_TYPE,constant("text/xml;charset=utf-8"))
+		.inOnly ("https4://".concat(endpoint))
+		.bean(ValeLog.class, "logging(" + EventCode.V100 + ", Finished)");
+
+	}
+}
--- src/main/java/br/com/vale/fis/routes/PostAPIManagementToTibcoEMS.java
+++ src/main/java/br/com/vale/fis/routes/PostAPIManagementToTibcoEMS.java
@@ -13,53 +13,67 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostSVAToAMQ extends RouteBuilder {
+public class PostAPIManagementToTibcoEMS extends RouteBuilder {
 
-	  @Value("${activemq.queue}")
-	  private String queueRequest;
+	@Value("${tibco.queueIn}")
+	private String tibcoQueueIn;
 
+	@Value("${tibco.host}")
+	private String host;
+
+	@Value("${tibco.user}")
+	private String user;
+
+	@Value("${tibco.password}")
+	private String password;
+
+	@Value("${azure.endpoint}")
+	private String endpoint;
+	
+	@Value("${azure.autorizationKey}")
+	private String autorizationKey;
+	
+	@Value("${azure.soapAction}")
+	private String soapAction;
+	
 	
 	@Override
 	public void configure() throws Exception {
 
+		getContext().addComponent("tibco", new ValeTibcoEMSComponent(host, user, password).getTibcoComponent());
 
 		onException(Exception.class).handled(true)
-				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
+				.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_XML_VALUE))
 				.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
 				.setBody(simple(createResponse("NOK", "${exception.message}")))
 				.bean(ValeLog.class, "logging(" + EventCode.E950 + ", ${exception.message})");
 
 		
-		rest("/sva") 
-		  .consumes(MediaType.APPLICATION_JSON_VALUE)
-		  .produces(MediaType.APPLICATION_JSON_VALUE)
-		  .post("/evento") 
-		  .route()
-		  .routeId("PostSvaSmartEventos")
+		rest("/location") .consumes(MediaType.APPLICATION_XML_VALUE)
+		  .produces(MediaType.APPLICATION_XML_VALUE) .post("/structure") .route()
+		  .routeId("PostCompanyStructure")
 		  .setHeader(ValeLogger.ROUTE_ID.getValue()).simple("${routeId}")
 		  .setHeader(ValeLogger.LOG_BODY.getValue()).simple("false")
 		  .bean(ValeLog.class, "logging(" + EventCode.V001 + ", Start)")
 		  .convertBodyTo(String.class, "UTF-8")
 		  .setHeader("CamelHttpCharacterEncoding", constant("UTF-8"))
-		  .to("amqOt:".concat(queueRequest))
-		  .bean(ValeLog.class, "logging(" + EventCode.V100 + ", Finished)");
-		
-		
-		rest("/sva") 
-		  .consumes(MediaType.APPLICATION_JSON_VALUE)
-		  .produces(MediaType.APPLICATION_JSON_VALUE)
-		  .post("/mensagens") 
-		  .route()
-		  .routeId("PostSvaSmartMensagen")
-		  .setHeader(ValeLogger.ROUTE_ID.getValue()).simple("${routeId}")
-		  .setHeader(ValeLogger.LOG_BODY.getValue()).simple("false")
-		  .bean(ValeLog.class, "logging(" + EventCode.V001 + ", Start)")
-		  .convertBodyTo(String.class, "UTF-8")
-		  .setHeader("CamelHttpCharacterEncoding", constant("UTF-8"))
-		  .to("amqOt:".concat(queueRequest))
+		  .inOnly("tibco:".concat(tibcoQueueIn)) 
 		  .bean(ValeLog.class, "logging(" + EventCode.V100 + ", Finished)");
 		 
 		
+		
+		rest("/organizational") .consumes(MediaType.APPLICATION_XML_VALUE)
+		  .produces(MediaType.APPLICATION_XML_VALUE) .post("/unit") .route()
+		  .routeId("PostOrganizationalUnit")
+		  .setHeader(ValeLogger.ROUTE_ID.getValue()).simple("${routeId}")
+		  .setHeader(ValeLogger.LOG_BODY.getValue()).simple("false")
+		  .bean(ValeLog.class, "logging(" + EventCode.V001 + ", Start)")
+		  .convertBodyTo(String.class, "UTF-8")
+		  .setHeader("CamelHttpCharacterEncoding", constant("UTF-8"))
+		  .inOnly("tibco:".concat(tibcoQueueIn)) 
+		  .bean(ValeLog.class, "logging(" + EventCode.V100 + ", Finished)");
+		 
+
 	}
 
 	private String createResponse(String status, String messageError) {
