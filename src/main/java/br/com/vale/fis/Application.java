3951b168a79857f5105a93fdb23592388402bbc5
João <c0645494@vale.com>
2023-09-12 13:39:58 -0300

    Remoção anotação

--- src/main/java/br/com/vale/fis/Application.java
+++ src/main/java/br/com/vale/fis/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:spring/*.xml"})
 public class Application {
 
   public static void main(final String[] args) {
